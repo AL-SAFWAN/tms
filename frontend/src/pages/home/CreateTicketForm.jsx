33673889cbed6220e15f2a63b4b8d174c88e9270
AL-SAFWAN <al-safwan.uddin@abehr.com>
2024-03-24 07:35:18 +0000

    remove icon from form

--- frontend/src/pages/home/CreateTicketForm.jsx
+++ frontend/src/pages/home/CreateTicketForm.jsx
@@ -63,16 +63,7 @@ export const Form = () => {
           <TextField
             formik={formik}
             label={'title'}
-            Icon={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                className="w-4 h-4 opacity-70"
-              >
-                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
-              </svg>
-            }
+            Icon={<div className="w-1 h-4 opacity-70"></div>}
           />
 
           <div className="text-base-content/70 p-1 font-thin capitalize">
